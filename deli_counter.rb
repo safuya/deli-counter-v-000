7644416698f7bb2d9591ed7d25e9dc552e092695
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 07:24:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,11 @@ def line(katz_deli)
   status = "The line is currently"
   if katz_deli.length == 0
     puts "#{status} empty."
+    return
   end
-
+  status += ":"
+  katz_deli.each_with_index do |name, index|
+    status += " #{index+1}. #{name}"
+  end
+  puts status
 end
